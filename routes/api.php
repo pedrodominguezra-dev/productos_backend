804e6f1b43dee17e2728d31a744036d9ab887e4f
Juan7633 <pedro.dominguez@bugarinlive.com>
2025-10-10 00:09:52 -0600

    Update renew token

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,16 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    $user = $request->user();
+    $newToken = $user->createToken('api_token')->plainTextToken;
+
+    return response()->json([
+        'user' => $user,
+        'token' => $newToken,
+    ]);
+})->middleware('auth:sanctum');
+
 
 // Login route
 Route::post('/login', [AuthController::class, 'login']);
